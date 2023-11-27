c2637a7fccde69cef89026155fe5cbe62d5b484d
LucentAI <contact.lucentai@gmail.com>
2023-11-26 22:07:56 -0600

    Commit

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-const { Client, Intents } = require('discord.js');
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const dotenv = require('dotenv');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+dotenv.config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages], partials: [Partials.USER, Partials.CHANNEL, Partials.GUILD_MEMBER] });
 
 const hardCommands = [
     {
         handler: require('./commands/handler.js'),
+        chatbot: require('./commands/chatbot.js'),
     }
 ];
 
 const fs = require('fs');
 
-client.on('ready', async () => {
-    const commands = JSON.parse(fs.readFileSync('commands.json', 'utf8'));
+const commands = JSON.parse(fs.readFileSync('commands.json', 'utf8'));
 
-    await client.application.commands.set(commands);
+client.on('ready', async () => {
+    for (const command of commands) {
+        console.log(`Registering command ${command.name}...`);
+        await client.application.commands.create(command);
+    }
 
     console.log('Global slash commands registered!');
 });
@@ -27,10 +34,21 @@ client.on('interactionCreate', async interaction => {
     const command = require(`${commands.find(cmd => cmd.name === commandName).path}`);
 
     try {
-        await command.execute(interaction);
+        await command(interaction, client);
     } catch (error) {
-        hardCommands[0].handler.execute(interaction, error);
+        // hardCommands[0].handler.execute(interaction, error);
+        console.error(error);
     }
 });
 
-client.login('YOUR_BOT_TOKEN');
+client.on('messageCreate', async message => {
+    if (message.content.startsWith(`<@${client.user.id}>`)) {
+        let msg = message.content.split('<@')[1].split('>')[1]
+        console.log(msg);
+        if (!msg) return message.channel.send('Yes?');
+
+        return hardCommands[0].chatbot(message, client, msg);
+    }
+});
+
+client.login(process.env.TOKEN);
